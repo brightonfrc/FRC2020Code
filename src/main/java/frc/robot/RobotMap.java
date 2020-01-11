5aac890250e1cab160481186048d767ad007f88a
Mikhail Nekrasov <misha.nekrasov@gmail.com>
2020-01-11 11:58:00 +0000

    Cleaned up unused files

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,8 +14,10 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  public static final int MOTOR_LEFT_1_ID = 0;
-  public static final int MOTOR_LEFT_2_ID = 1;
-  public static final int MOTOR_RIGHT_1_ID = 2;
-  public static final int MOTOR_RIGHT_2_ID = 3;
+  public static final int MOTOR_LEFT_0_ID = 0;
+  public static final int MOTOR_LEFT_1_ID = 1;
+  public static final int MOTOR_LEFT_2_ID = 2;
+  public static final int MOTOR_RIGHT_0_ID = 3;
+  public static final int MOTOR_RIGHT_1_ID = 4;
+  public static final int MOTOR_RIGHT_2_ID = 5;
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,7 +7,7 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Talon;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
@@ -16,10 +16,12 @@ import frc.robot.RobotMap;
  * Add your docs here.
  */
 public class DriveTrain extends Subsystem {
-  TalonSRX motorLeft1 = new TalonSRX(RobotMap.MOTOR_LEFT_1_ID);
-  TalonSRX motorLeft2 = new TalonSRX(RobotMap.MOTOR_LEFT_2_ID);
-  TalonSRX motorRight1 = new TalonSRX(RobotMap.MOTOR_RIGHT_1_ID);
-  TalonSRX motorRight2 = new TalonSRX(RobotMap.MOTOR_RIGHT_2_ID);
+  private Talon motorLeft0 = new Talon(RobotMap.MOTOR_LEFT_0_ID);
+  private Talon motorLeft1 = new Talon(RobotMap.MOTOR_LEFT_1_ID);
+  private Talon motorLeft2 = new Talon(RobotMap.MOTOR_LEFT_2_ID);
+  private Talon motorRight0 = new Talon(RobotMap.MOTOR_RIGHT_0_ID);
+  private Talon motorRight1 = new Talon(RobotMap.MOTOR_RIGHT_1_ID);
+  private Talon motorRight2 = new Talon(RobotMap.MOTOR_RIGHT_2_ID);
   
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
